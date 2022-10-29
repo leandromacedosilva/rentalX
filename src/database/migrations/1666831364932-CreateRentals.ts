eeeceb1a4a473ecc17fc8d023d43ecdff23737b6
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-10-29 09:41:21 -0300

    updated file 1666831364932-CreateRentals.ts in directory MIGRATIONS

--- src/database/migrations/1666831364932-CreateRentals.ts
+++ src/database/migrations/1666831364932-CreateRentals.ts
@@ -1,7 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateRentals1666831364932 implements MigrationInterface {
-
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
@@ -12,10 +11,33 @@ export class CreateRentals1666831364932 implements MigrationInterface {
                         type: "uuid",
                         isPrimary: true,
                     },
-                    {}
-                ]
+                    {
+                        name: "start_date",
+                        type: "date",
+                    },
+                    {
+                        name: "end_date",
+                        type: "date",
+                    },
+                    {
+                        name: "expected_return_date",
+                        type: "date",
+                    },
+                    {
+                        name: "total",
+                        type: "number",
+                    },
+                    {
+                        name: "created_at",
+                        type: "date",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "date",
+                    },
+                ],
             })
-        )
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
